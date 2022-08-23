3e9a05fbb00a74dcd28b290d7a06057c44cb5d9c
majaliju <majalijunyc@gmail.com>
2022-08-23 19:38:01 -0400

    commit tree be186423513f895b45bbd3e52501cfe692a43a6a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: {in: %w{Fiction Non-Fiction}}
+  validate :clickbait?
+
+  def clickbaiter
+    unless title.match(/(Won't Believe)|(Top \d)|(Secret)|(Guess)/)
+      errors.add(:title, "Come on you know how this business works!")
+  end
+
+  def clickbait?
+    
+    unless title && title.match(/(Won't Believe)|(Top \d)|(Secret)|(Guess)/)
+ 
+     errors.add(:title, "not clickbait")
+
+end
+
+  
 end
